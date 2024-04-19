432fa8374368d7fb1e192a344c0a64c9c0806fa3
qiangwangmao <uss03word792fdx@gmail.com>
2024-04-19 16:25:03 +0200

    yea

--- help.c
+++ help.c
@@ -3,6 +3,7 @@
 int help() {
     puts("Available commands:");
     puts("  help (Lists all available commands)");
+    puts("  clear (Clears the terminal)");
     puts("  write   (Writes data to a file)");
     puts("  hostname (Change your hostname)");
     puts("  shutdown");
